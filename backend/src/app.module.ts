df835003ab6bd89b7ecbc201c56a8f78fb569a05
yosko99 <thedriftmaniaka1997@gmail.com>
2022-11-20 15:16:58 +0200

    Connect to mysql DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
-import { AppGateway } from './app/app.gateway';
+
+import { UsersModule } from './modules/users/users.module';
+
+import { SocketGateway } from './socket/socket.gateway';
+
+const { DB_HOSTNAME, DB_PASSWORD, DB_PORT, DB_USERNAME, DB_DATABASE } =
+  process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DB_HOSTNAME,
+      port: DB_PORT as unknown as number,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService, SocketGateway],
 })
 export class AppModule {}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
+import io from 'socket.io-client';
 
 import GlobalCSS from './styles/global.css';
 
+const socket = io('ws://localhost:5000');
+
+interface ClientInterface {
+  id: string,
+  name: string
+}
+
 function App () {
+  const [onlineUsers, setOnlineUsers] = useState<[ClientInterface]>([{
+    id: '',
+    name: ''
+  }]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      socket.emit('connected', prompt('enter name'));
+    });
+
+    socket.on('online', (data: {online: [ClientInterface]}) => {
+      setOnlineUsers([...data.online]);
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <GlobalCSS />
+      {onlineUsers!.length > 0 && onlineUsers!.map((user, index: number) => (
+        <h1 key={index}>{user.name}</h1>
+      ))}
     </BrowserRouter>
   );
 }
